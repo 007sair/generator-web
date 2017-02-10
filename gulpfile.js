3345924cb2e7fe2d71ca1d49112e07d2dcc23ef2
longchan <longchan@mia.com>
2017-02-10 18:49:21 +0800

    use gulp+webpack

--- gulpfile.js
+++ gulpfile.js
@@ -1,109 +1,146 @@
-(function(){
+/**
+ * 脚手架项目
+ * create by lc
+ */
 
-	//基础包
-	var gulp = require("gulp");
-	var sass = require('gulp-sass');
-	var sourcemaps = require('gulp-sourcemaps');
+var gulp = require('gulp'),
+    os = require('os'),
+    path = require('path'),
+    gutil = require('gulp-util'),
+    sass = require('gulp-sass'),
+    concat = require('gulp-concat'),
+    gulpOpen = require('gulp-open'),
+    uglify = require('gulp-uglify'),
+    cleanCSS = require('gulp-clean-css'),
+    md5 = require('gulp-md5-plus'),
+    fileinclude = require('gulp-file-include'),
+    clean = require('gulp-clean'),
+    spriter = require('gulp-css-spriter'),
+    webpack = require('webpack'),
+    webpackConfig = require('./webpack.config.js'),
+    connect = require('gulp-connect');
 
-	/**
-	 * JS
-	 */
-	var concat = require('gulp-concat');
-	var uglify = require("gulp-uglify");
-	var rename = require('gulp-rename');
-	
-	/**
-	 * CSS
-	 */
-	var cleanCSS = require('gulp-clean-css');
-	//PostCSS
-	var postcss = require('gulp-postcss');
-	var autoprefixer = require('autoprefixer');
-	var processors = [
-		autoprefixer({
-			browsers: ['ie >= 9', 'Chrome >= 20', 'Android >= 3.0', 'Firefox >= 10']
-		})
-	];
+var rev = require('gulp-rev');                                  //- 对文件名加MD5后缀
+var revCollector = require('gulp-rev-collector');               //- 路径替换
+var gulpSequence = require('gulp-sequence');   					//- gulp串行任务
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+var sourcemaps = require('gulp-sourcemaps');
 
-	//增强
-	var browserSync = require('browser-sync').create();
-	var reload = browserSync.reload;
-
-	var changed = require('gulp-changed');
-	var replace = require('gulp-replace');
-	var filter  = require('gulp-filter');
-
-
-	var paths = {
-		root: './',
-		src: 'src/',
-		dist: 'dist/',
-		sass: 'sass/',
-		css: 'css/'
-	};
-
-
-	/**
-	 * sass转css任务
-	 * 生成css.css & css.min.css文件
-	 */
-	gulp.task('sass', function() {
-		return gulp.src(paths.sass + '**/*.scss')
-			.pipe(sourcemaps.init())
-			.pipe(sass({
-				outputStyle: 'compact'
-			}).on('error', sass.logError))
-			.pipe(postcss(processors))
-			.pipe(cleanCSS({
-				keepBreaks: true,
-				compatibility: 'ie7',
-				advanced: false
-			}))
-			.pipe(sourcemaps.write('.map/'))
-			.pipe(gulp.dest(paths.css))
-			.pipe(filter(paths.css + '**/*.css')) // Filtering stream to only css files
-			.pipe(reload({
-				stream: true
-			})) //need browserSync
-			.pipe(gulp.dest(paths.css))
-			.pipe(cleanCSS({
-				keepBreaks: false,
-				compatibility: 'ie7'
-			}))
-			.pipe(rename(function(path) {
-				path.basename += '.min';
-			}))
-			.pipe(gulp.dest(paths.css));
-	});
-
-	gulp.task('script', function(){
-		return gulp.src(paths.src + '**/*.js')
-			.pipe(uglify())
-			.pipe(rename({
-				suffix : '.min'
-			}))
-			.pipe(gulp.dest(paths.dist));
+var processors = [
+	autoprefixer({
+		browsers: ['ie >= 9', 'Chrome >= 20', 'Android >= 3.0', 'Firefox >= 10']
 	})
+];
+
+var isProduction = gutil.env._[0] == 'dev';
 
 
-	gulp.task('default', function() {
-		//指定文件，用browserSync监听文件变化
-		browserSync.init({
-			server: {
-	            baseDir: "./"
-	        }
-		});
+var host = {
+    path: 'dist/',
+    port: 3000,
+    html: 'index.html'
+};
 
-		//监听文件变化并执行sass任务
-		gulp.watch(paths.sass + '**/*.scss', ['sass']);
+//mac chrome: "Google chrome", 
+var browser = os.platform() === 'linux' ? 'Google chrome' : (
+  os.platform() === 'darwin' ? 'Google chrome' : (
+  os.platform() === 'win32' ? 'chrome' : 'firefox'));
+var pkg = require('./package.json');
 
-		gulp.watch(paths.src + '**/*.js', ['script']);
+//压缩合并css
+gulp.task('sassmin', function (done) {
+    if (isProduction) { //dev
+        gulp.src(['src/css/sass/main.scss', 'src/css/style.css'], {base: 'src/css/'})
+            .pipe(sourcemaps.init())
+            .pipe(sass())
+            .pipe(postcss(processors))
+            .pipe(sourcemaps.write('.'))
+            .pipe(gulp.dest('dist/css/'))
+            .pipe(connect.reload())
+            .on('end', done)
+    } else {
+        gulp.src(['src/css/sass/main.scss', 'src/css/style.css'], {base: 'src/css/'})
+            .pipe(sass())
+            .pipe(postcss(processors))
+            .pipe(cleanCSS())
+            .pipe(rev())
+            .pipe(gulp.dest('./dist/css'))
+            .pipe(rev.manifest())
+            .pipe(gulp.dest('./rev'))
+            .on('end', done);
+    }
+});
 
-		//监听html发生变化时手动重载浏览器
-		gulp.watch("*.html").on("change", browserSync.reload);
-
-	});
 
 
+gulp.task('sprite', function(){
+	console.log('task sprite')
+})
 
-})();
+gulp.task('clean', function (done) {
+    gulp.src(['dist', 'rev'])
+        .pipe(clean());
+    done();
+});
+
+gulp.task('html', function(done){
+    gulp.src('src/**/*.html')
+        .pipe(gulp.dest('./dist'))
+        .pipe(connect.reload())
+    //done()
+})
+
+gulp.task('reload', function(){
+    gulp.src('src/**/*')
+        .pipe(connect.reload())
+})
+
+
+gulp.task('rev', ['sassmin'], function(done) {
+    return gulp.src(['./rev/*.json', './src/**/*.html'])   		//- 读取 rev-manifest.json 文件以及需要进行css名替换的文件
+        .pipe(revCollector())                           //- 执行文件内css名的替换
+        .pipe(gulp.dest('dist/'))                    	//- 替换后的文件输出的目录
+    //done()
+});
+
+
+gulp.task('watch', function (done) {
+    gulp.watch('src/**/*.scss', ['sassmin']).on('change', function(event){
+        console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
+    });
+    gulp.watch('src/**/*.html', ['html']).on('change', function(event){
+        console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
+    });
+});
+
+
+gulp.task('connect', function () {
+    connect.server({
+        root: host.path,
+        port: host.port,
+        livereload: true
+    });
+});
+
+gulp.task('open', function (done) {
+    gulp.src('')
+        .pipe(gulpOpen({
+            app: browser,
+            uri: 'http://localhost:3000/'
+        }))
+    //done()
+});
+
+//gulpSequence：圆括号串行，中括号并行
+
+//发布
+gulp.task('build', function(cb) {
+    gulpSequence('clean', 'rev', ['connect', 'open'], cb);
+});
+
+//开发
+gulp.task('dev', function(cb) {
+    gulpSequence('clean', 'sassmin', ['html', 'connect', 'open'], 'watch', cb);
+});
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,49 @@
+var CommonsChunkPlugin = require("webpack/lib/optimize/CommonsChunkPlugin");
+var path = require('path');
 var webpack = require('webpack');
-var ExtractTextPlugin = require('extract-text-webpack-plugin'); //css样式从js文件中分离出来,需要通过命令行安装 extract-text-webpack-plugin依赖包
-var autoprefixer = require('autoprefixer');
+var fs = require('fs');
+var uglifyJsPlugin = webpack.optimize.UglifyJsPlugin;
 
-/*
- * plugins 是插件项
- * entry 是页面入口文件配置，output 是对应输出项配置（即入口文件最终要生成什么名字的文件、存放到哪里）
- */
+var srcDir = path.resolve(process.cwd(), 'src');
 
-//todo postcss/localhost
+//获取多页面的每个入口文件，用于配置中的entry
+function getEntry() {
+    var jsPath = path.resolve(srcDir, 'js');
+    var dirs = fs.readdirSync(jsPath);
+    var matchs = [], files = {};
+    dirs.forEach(function (item) {
+        matchs = item.match(/(.+)\.js$/);
+        if (matchs) {
+            files[matchs[1]] = path.resolve(srcDir, 'js', item);
+        }
+    });
+    // console.log(JSON.stringify(files));
+    return files;
+}
 
 module.exports = {
-	entry: './index.js',
-	output: {
-		filename: 'webpack/bundle.js'
-	},
-	module: {
-		loaders: [
-			//.css 文件使用 style-loader 和 css-loader 来处理
-            { test: /\.css$/, loader: ExtractTextPlugin.extract("style", 'css')},
-            //解析.scss文件,对于用 import 或 require 引入的sass文件进行加载，以及<style lang="sass">...</style>声明的内部样式进行加载
-			//.scss 文件使用 style-loader、css-loader 和 sass-loader 来编译处理
-			//这里用了样式分离出来的插件，如果不想分离出来，可以直接这样写 loader:'style!css!sass'
-			{ test: /\.scss$/, loader: ExtractTextPlugin.extract("style", 'css!sass')}
-		]
-	},
-	plugins: [
-		new ExtractTextPlugin("webpack/style.css"), //提取出来的样式放在style.css文件中
-		new webpack.optimize.UglifyJsPlugin({ //代码压缩 其实并不能说是在压缩css代码，本质来说还是压缩js代码，再将这块代码输出到css文件中。
-			compress: {
-				warnings: false
-			}
-		})
-	]
-}	
+    cache: true,
+    devtool: "source-map",
+    entry: getEntry(),
+    output: {
+        path: path.join(__dirname, "dist/js/"),
+        publicPath: "dist/js/",
+        filename: "[name].js",
+        chunkFilename: "[chunkhash].js"
+    },
+    resolve: {
+        alias: {
+            jquery: srcDir + "/js/lib/jquery.min.js",
+            core: srcDir + "/js/core",
+            ui: srcDir + "/js/ui"
+        }
+    },
+    plugins: [
+        new CommonsChunkPlugin('common.js'),
+        new uglifyJsPlugin({
+            compress: {
+                warnings: false
+            }
+        })
+    ]
+};
