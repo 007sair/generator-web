247577397b5cf841b85269448aa088cca1ee88ec
007sair <007sair@163.com>
2017-11-30 17:24:22 +0800

    add mac sh

---
+++ sprite.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cp -rf spritesmith/spritesheet-templates.js node_modules/gulp.spritesmith/node_modules/spritesheet-templates/lib/spritesheet-templates.js
+cp -rf spritesmith/templates/scss.template.handlebars node_modules/gulp.spritesmith/node_modules/spritesheet-templates/lib/templates/scss.template.handlebars
